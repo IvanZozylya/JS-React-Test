ff91c25c980aaafbdff971c331a54894691a965a
Ivan Zozylya <zozylya.ivan@gmail.com>
2023-11-23 17:52:49 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+// const result = confirm("Are you here?");
+// console.log(result);
+// const answer = +prompt("How old are you?", "18");
+// console.log(typeof answer);
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваша фамилия?", "");
+// answers[2] = +prompt("Сколько вам лет?", "");
+
+// console.log(typeof answers);
+
+// const category = "toys";
+// console.log(`https://someurl.com/${category}/5`);
+
+let incr = 10;
+let decr = 10;
+
+// incr++;
+// decr--;
+console.log(++incr);
+console.log(--decr);
